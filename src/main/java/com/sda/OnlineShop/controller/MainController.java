200734c2ba5e1a103ee8b172585b2773667e0243
Bogdan Bucea <bogdanbucea321@gmail.com>
2023-02-15 18:42:28 +0200

    test

--- src/main/java/com/sda/OnlineShop/controller/MainController.java
+++ src/main/java/com/sda/OnlineShop/controller/MainController.java
@@ -2,6 +2,7 @@ package com.sda.OnlineShop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MainController {
@@ -9,6 +10,11 @@ public class MainController {
     public String addProductGet(){
     //teoretic aici executam "bussiness logic"
     //dupa intoarcem un nume de pagina
+        return "addProduct";
+    }
+    @PostMapping("/addProduct")
+    public String addProductPost(){
+        System.out.println("S-a apelat add product!");
         return "addProduct";
     }
 }
